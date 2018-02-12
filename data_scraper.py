6296306dc557d2d5a1a8ffae1cbee9764bc5d5db
samreay <samuelreay@gmail.com>
2018-02-13 08:29:44 +1000

    dont fail when the wifi drops out

--- data_scraper.py
+++ data_scraper.py
@@ -10,17 +10,10 @@ class DateTimeEncoder(json.JSONEncoder):
             return o.isoformat()
         return json.JSONEncoder.default(self, o)
 
-client = get_client()
-coins = get_trade_coins()
-symbols = [c + "BTC" for c in coins]
 
-while True:
+def get_info(client, symbols):
     depth_raw = {}
     tfhour = {}
-    now = datetime.now()
-    filename = now.isoformat().replace(":", "_")
-    print(filename)
-    
     prices_raw = client.get_all_tickers()
     for s in symbols:
         print(s)
@@ -28,9 +21,24 @@ while True:
         time.sleep(0.1)
         tfhour[s] = client.get_ticker(symbol=s)
         time.sleep(0.1)
+    return prices_raw, depth_raw, tfhour
 
-    data = {"prices": prices_raw, "depth_raw": depth_raw, "time": filename, "tfhour": tfhour}
-    with open("raw_data/%s.txt" % filename, "w") as f:
-        json.dump(data, f, cls=DateTimeEncoder)
-    
-    time.sleep(120)
+if __name__ == "__main__":
+    client = get_client()
+    coins = get_trade_coins()
+    symbols = [c + "BTC" for c in coins]
+
+    while True:
+
+        now = datetime.now()
+        filename = now.isoformat().replace(":", "_")
+        print(filename)
+        try:
+            prices_raw, depth_raw, tfhour = get_info(client, symbols)
+            data = {"prices": prices_raw, "depth_raw": depth_raw, "time": filename, "tfhour": tfhour}
+            with open("raw_data/%s.txt" % filename, "w") as f:
+                json.dump(data, f, cls=DateTimeEncoder)
+        except Exception as e:
+            print(e)
+
+        time.sleep(120)
